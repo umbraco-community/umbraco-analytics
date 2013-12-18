6d7722f531579ed02c4e25e1a88e0d26e2c71522
mvanhelmont <marcel@helmont.nl>
2013-12-18 22:49:22 +0100

    OAuth Code Flow

--- Analytics.Site/App_Plugins/Analytics/backOffice/AnalyticsTree/controllers/Settings.Controller.js
+++ Analytics.Site/App_Plugins/Analytics/backOffice/AnalyticsTree/controllers/Settings.Controller.js
@@ -1,5 +1,5 @@
 ﻿angular.module("umbraco").controller("Analytics.SettingsController",
-    function ($scope, settingsResource, notificationsService) {
+    function ($scope, $window, $location, settingsResource, notificationsService) {
 
         //Get all settings via settingsResource - does WebAPI GET call
         settingsResource.getall().then(function (response) {
@@ -13,23 +13,21 @@
         //Auth - Click
         $scope.auth = function () {
 
+            // TODO Load settings from settings object
+            var clientID = "805395301940-duj60glgp6uat7v23lu084bf6arcag4j.apps.googleusercontent.com";
+            var redirectUri = "http://localhost:62315/App_Plugins/Analytics/backoffice/oAuth.aspx";
+
             //Open a dialog window to oAuth
-            window.open("http://google.com", "oAuthAnayltics", "location=0,status=0,width=800,height=400");
+            var popup = window.open("https://accounts.google.com/o/oauth2/auth?response_type=code&scope=https://www.googleapis.com/auth/analytics.readonly&redirect_uri=" + redirectUri + "&client_id=" + clientID + "&approval_prompt=force&include_granted_scopes=true&access_type=offline", "oAuthAnayltics", "location=0,status=0,width=800,height=400");
 
-            //It will callback to http://analytics.com/oAuth/callback or similar
 
-            //Once we have got token at anlytics.com site in popup
-
-            //It does a WebAPI post to the Umbraco Site
-            //http://localhost:62315/umbraco/Analytics/SettingsApi/PostSettingValue?key=refreshToken&value=blah
-
-            //Once got OK back from WebAPI that the setting file has been posted/updated
-
-            //Close Popup Window
-
-            //Set showAuth to false
-
-            //Maybe show a sucess message with ng-show perhaps in it's place?
+            angular.element(window).bind('message', function (event) {
+                event = event.originalEvent || event;
+                if (event.source == popup && event.origin == window.location.origin) {
+                    //TODO Way to update the view
+                    $window.location.reload();
+                }
+            });
         };
 
         //Save - click...
