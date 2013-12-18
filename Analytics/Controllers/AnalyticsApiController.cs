9cc8d5db2b5e9b7b5633f2e2d9d5d97b7b605f71
Anders Bjerner <abjerner@skybrud.dk>
2013-12-19 00:30:05 +0100

    Implemented a few controller methods - we can now pull real data from the Google Analytics API :D

--- Analytics/Controllers/AnalyticsApiController.cs
+++ Analytics/Controllers/AnalyticsApiController.cs
@@ -1,10 +1,17 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Google.Apis.Analytics.v3;
 using Google.Apis.Analytics.v3.Data;
 using Google.Apis.Auth.OAuth2.Mvc;
 using Google.Apis.Services;
+using Skybrud.Social.Google;
+using Skybrud.Social.Google.Analytics;
+using Skybrud.Social.Google.Analytics.Objects;
+using Skybrud.Social.Google.Analytics.Responses;
 using Umbraco.Web.Mvc;
 using Umbraco.Web.WebApi;
 
@@ -17,7 +24,9 @@ namespace Analytics.Controllers
         private const string ApplicationName    = "Analytics for Umbraco";
         private const string ClientID           = "206456221167-1cm2983v0d6i5emt132316ommn6hsv8j.apps.googleusercontent.com";
 
-
+        private GoogleService GetGoogleService() {
+            return GoogleService.CreateFromRequestToken(AnalyticsConfig.ClientId, AnalyticsConfig.ClientSecret, AnalyticsConfig.RefreshToken);
+        }
 
         public async Task<RealtimeData> GetRealTimeStats(CancellationToken cancellationToken)
         {
@@ -43,5 +52,43 @@ namespace Analytics.Controllers
 
             return null;
         }
+
+        public HttpResponseMessage GetAccounts() {
+
+            // Get the accounts from the Google Analytics API
+            AnalyticsAccount[] accounts = GetGoogleService().Analytics.GetAccounts().Items;
+
+            // Return the accounts as JSON
+            return Request.CreateResponse(HttpStatusCode.OK, accounts, Configuration.Formatters.JsonFormatter);
+
+        }
+
+        public HttpResponseMessage GetProfiles() {
+
+            // Get the profiles from the Google Analytics API
+            AnalyticsProfile[] profiles = GetGoogleService().Analytics.GetProfiles().Items;
+
+            // Return the profiles as JSON
+            return Request.CreateResponse(HttpStatusCode.OK, profiles, Configuration.Formatters.JsonFormatter);
+
+        }
+
+        public HttpResponseMessage GetVisits(string profile) {
+
+            // Get the visits from the Google Analytics API
+            AnalyticsDataResponse data = GetGoogleService().Analytics.GetData(
+                profile,
+                DateTime.Now.Subtract(TimeSpan.FromDays(31)),
+                DateTime.Now,
+                new [] { AnalyticsMetrics.Visits, AnalyticsMetrics.Pageviews },
+                new[] { AnalyticsDimension.Date }
+            );
+
+            // Return the data as JSON
+            return Request.CreateResponse(HttpStatusCode.OK, data, Configuration.Formatters.JsonFormatter);
+
+        }
+    
     }
+
 }
