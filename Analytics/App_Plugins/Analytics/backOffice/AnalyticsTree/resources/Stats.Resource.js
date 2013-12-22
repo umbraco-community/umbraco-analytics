10a7b9b205ecc4315409dec67a71da9752723532
Warren Buckley <warren@creativewebspecialist.co.uk>
2013-12-22 20:30:39 +0000

    WIP: Trying to debug account & profile for settings.

--- Analytics/App_Plugins/Analytics/backOffice/AnalyticsTree/resources/Stats.Resource.js
+++ Analytics/App_Plugins/Analytics/backOffice/AnalyticsTree/resources/Stats.Resource.js
@@ -5,51 +5,51 @@ angular.module("umbraco.resources")
             
             //TODO: Get Profile ID from saved profile in settings
 
-            getlanguage: function () {
+            getlanguage: function (profileID) {
                 return $http.get("Analytics/AnalyticsApi/GetLanguage?profile=95037");
             },
 
-            getcountries: function () {
+            getcountries: function (profileID) {
                 return $http.get("Analytics/AnalyticsApi/GetCountry?profile=95037");
             },
 
-            getbrowsers: function () {
+            getbrowsers: function (profileID) {
                 return $http.get("Analytics/AnalyticsApi/GetBrowser?profile=95037");
             },
 
-            getbrowserspecifics: function () {
+            getbrowserspecifics: function (profileID) {
                 return $http.get("Analytics/AnalyticsApi/GetBrowserVersion?profile=95037");
             },
 
-            getdevicetypes: function () {
+            getdevicetypes: function (profileID) {
                 return $http.get("Analytics/AnalyticsApi/GetDeviceTypes?profile=95037");
             },
 
-            getdevices: function () {
+            getdevices: function (profileID) {
                 return $http.get("Analytics/AnalyticsApi/GetDevices?profile=95037");
             },
 
-            getresolutions: function () {
+            getresolutions: function (profileID) {
                 return $http.get("Analytics/AnalyticsApi/GetScreenRes?profile=95037");
             },
 
-            getos: function () {
+            getos: function (profileID) {
                 return $http.get("Analytics/AnalyticsApi/GetOperatingSystems?profile=95037");
             },
 
-            getsocialnetworks: function () {
+            getsocialnetworks: function (profileID) {
                 return $http.get("Analytics/AnalyticsApi/GetSocialNetworkSources?profile=95037");
             },
 
-            getkeywords: function () {
+            getkeywords: function (profileID) {
                 return $http.get("Analytics/AnalyticsApi/GetKeywords?profile=95037");
             },
 
-            getvisits: function () {
+            getvisits: function (profileID) {
                 return $http.get("Analytics/AnalyticsApi/GetVisits?profile=95037");
             },
 
-            getsources: function () {
+            getsources: function (profileID) {
                 return $http.get("Analytics/AnalyticsApi/GetSources?profile=95037");
             },
 
