f559c80248d27189473fa67f8846d6b422abb1b0
Warren Buckley <warren@creativewebspecialist.co.uk>
2014-01-02 19:19:33 +0000

    WIP: Country view use Google's GeoChart (need to use rela data next)

--- Analytics/App_Plugins/Analytics/backOffice/AnalyticsTree/controllers/Country.Controller.js
+++ Analytics/App_Plugins/Analytics/backOffice/AnalyticsTree/controllers/Country.Controller.js
@@ -1,18 +1,50 @@
 ﻿angular.module("umbraco").controller("Analytics.CountryController",
-    function ($scope, statsResource, settingsResource) {
+    function ($scope, statsResource, settingsResource, assetsService) {
 
         var profileID = "";
 
-        //Get Profile
-        settingsResource.getprofile().then(function(response) {
-            $scope.profile = response.data;
-            profileID = response.data.Id;
 
-            //Get Countires via statsResource - does WebAPI GET call
-            statsResource.getcountries(profileID).then(function (response) {
-                $scope.data = response.data;
+        assetsService.loadJs('http://www.google.com/jsapi')
+            .then(function () {
+                google.load("visualization", "1",
+                {
+                    callback: initChart,
+                    packages: ['geochart']
+                });
             });
 
-        });
+        function initChart() {
+            //Get Profile
+            settingsResource.getprofile().then(function(response) {
+                $scope.profile = response.data;
+                profileID = response.data.Id;
+
+                //Get Countires via statsResource - does WebAPI GET call
+                statsResource.getcountries(profileID).then(function(response) {
+                    $scope.data = response.data;
+
+
+                    var chartMapData = google.visualization.arrayToDataTable([
+                              ['Country', 'Page Views'],
+                              ['Germany', 200],
+                              ['United States', 300],
+                              ['Brazil', 400],
+                              ['Canada', 500],
+                              ['France', 600],
+                              ['RU', 700]
+                    ]);
+
+                    //Options for map (currently use defaults)
+                    var options = {};
+
+                    //Create the GeoChart with the countryChart DIV
+                    var geochart = new google.visualization.GeoChart(document.getElementById('countryChart'));
+
+                    //Draw the chart with the data & options
+                    geochart.draw(chartMapData, options);
+
+                });
+            });
+        };
 
     });
