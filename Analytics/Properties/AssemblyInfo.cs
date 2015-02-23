7be0a80ea64f262f04db944b560dcc55c876ac3b
Warren Buckley <warren@creativewebspecialist.co.uk>
2015-02-23 10:26:16 +0000

    Update Assembly Info for DLL so more descriptive & mentions Umbraco

--- Analytics/Properties/AssemblyInfo.cs
+++ Analytics/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Analytics")]
+[assembly: AssemblyTitle("Analytics for Umbraco CMS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("The Umbraco Community")]
