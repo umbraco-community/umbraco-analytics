a6e40247f5ff98472852b23a7e65423310881c30
Jeavon Leopold <jeavon@crumpled-dog.com>
2015-02-27 16:04:59 +0000

    Adding single MSBuild to create both Umbraco and NuGet packages for AppVeyor

--- Analytics/Properties/AssemblyInfo.cs
+++ Analytics/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("The Umbraco Community")]
 [assembly: AssemblyProduct("Analytics")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright Warren Buckley 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+// [assembly: AssemblyVersion("0.1.0.100")]
+[assembly: AssemblyVersion("0.1.0.100")]
+[assembly: AssemblyFileVersion("0.1.0.100")]
+[assembly: AssemblyInformationalVersion("0.1.0-build100")]
--- BuildPackage/Build.bat
+++ BuildPackage/Build.bat
@@ -1,5 +1,6 @@
 ECHO APPVEYOR_REPO_BRANCH: %APPVEYOR_REPO_BRANCH%
+ECHO APPVEYOR_REPO_TAG: %APPVEYOR_REPO_TAG%
 ECHO APPVEYOR_BUILD_NUMBER : %APPVEYOR_BUILD_NUMBER%
 ECHO APPVEYOR_BUILD_VERSION : %APPVEYOR_BUILD_VERSION%
-
-Call "C:\Program Files (x86)\MSBuild\12.0\Bin\MsBuild.exe" Package.build.xml
+Call Tools\nuget.exe restore ..\Analytics.sln
+Call "%programfiles(x86)%\MSBuild\12.0\Bin\MsBuild.exe" package.proj
