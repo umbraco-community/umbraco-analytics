69775f64126c5dd6476cb3a9fcd4423b2f17718a
Warren Buckley <warren@creativewebspecialist.co.uk>
2015-02-27 10:58:58 +0000

    Add in Nuget NuSpec file for AppVeyor

--- Analytics/Properties/AssemblyInfo.cs
+++ Analytics/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Analytics for Umbraco CMS")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("This is an extension for the Umbraco CMS to allow you to view your Google Analytics inside the CMS")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("The Umbraco Community")]
 [assembly: AssemblyProduct("Analytics")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
