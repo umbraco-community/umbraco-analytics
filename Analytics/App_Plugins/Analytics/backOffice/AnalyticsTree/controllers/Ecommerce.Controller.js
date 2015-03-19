08f4ea467fe17e4383422d950c2723923cb4c14d
bjarnef <bjarne_fyrstenborg@hotmail.com>
2015-03-20 00:10:10 +0100

    Highlight current tree

--- Analytics/App_Plugins/Analytics/backOffice/AnalyticsTree/controllers/Ecommerce.Controller.js
+++ Analytics/App_Plugins/Analytics/backOffice/AnalyticsTree/controllers/Ecommerce.Controller.js
@@ -1,5 +1,5 @@
 ﻿angular.module("umbraco").controller("Analytics.EcommerceController",
-    function ($scope, $location, statsResource, settingsResource) {
+    function ($scope, $location, $routeParams, statsResource, settingsResource, navigationService) {
 
         var profileID = "";
 
@@ -94,4 +94,6 @@
 
             });
         });
+
+        navigationService.syncTree({ tree: 'analyticsTree', path: ["-1", $routeParams.id], forceReload: false });
     });
