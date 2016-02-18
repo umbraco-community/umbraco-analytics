0972a37a02904fb53e5ec255985ba984fcea8947
Heather Floyd <hfloyd@scandiaconsulting.com>
2016-02-18 16:25:04 -0500

    Moving Tree nodes definitions to a new config file

--- Analytics/Controllers/AnalyticsTreeController.cs
+++ Analytics/Controllers/AnalyticsTreeController.cs
@@ -10,10 +10,15 @@ using umbraco.BusinessLogic.Actions;
 
 namespace Analytics.Controllers
 {
+    using System.Web.Hosting;
+    using System.Xml;
+
     [Tree("analytics", "analyticsTree", "Analytics")]
     [PluginController("Analytics")]
     public class AnalyticsTreeController : TreeController
     {
+        private const string TreeNodesPath = "~/App_Plugins/Analytics/treenodes.config";
+
         /// <summary>
         /// 
         /// </summary>
@@ -33,17 +38,30 @@ namespace Analytics.Controllers
 
                 //Add nodes
                 var treeNodes = new List<SectionTreeNode>();
-                treeNodes.Add(new SectionTreeNode() { Id = "views", Title = ui.Text("analytics", "views"), Icon = "icon-activity", Route = string.Format("{0}/view/{1}", mainRoute, "views") });
-                treeNodes.Add(new SectionTreeNode() { Id = "keywords", Title = ui.Text("analytics", "keywords"), Icon = "icon-tags", Route = string.Format("{0}/view/{1}", mainRoute, "keywords") });
-                treeNodes.Add(new SectionTreeNode() { Id = "social", Title = ui.Text("analytics", "socialNetwork"), Icon = "icon-chat-active", Route = string.Format("{0}/view/{1}", mainRoute, "social") });
-                treeNodes.Add(new SectionTreeNode() { Id = "os", Title = ui.Text("analytics", "operatingSystem"), Icon = "icon-windows", Route = string.Format("{0}/view/{1}", mainRoute, "os") });
-                treeNodes.Add(new SectionTreeNode() { Id = "screenres", Title = ui.Text("analytics", "screenResolution"), Icon = "icon-display", Route = string.Format("{0}/view/{1}", mainRoute, "screenres") });
-                treeNodes.Add(new SectionTreeNode() { Id = "devices", Title = ui.Text("analytics", "devices"), Icon = "icon-iphone", Route = string.Format("{0}/view/{1}", mainRoute, "devices") });
-                treeNodes.Add(new SectionTreeNode() { Id = "browser", Title = ui.Text("analytics", "browser"), Icon = "icon-browser-window", Route = string.Format("{0}/view/{1}", mainRoute, "browser") });
-                treeNodes.Add(new SectionTreeNode() { Id = "language", Title = ui.Text("analytics", "language"), Icon = "icon-chat-active", Route = string.Format("{0}/view/{1}", mainRoute, "language") });
-                treeNodes.Add(new SectionTreeNode() { Id = "country", Title = ui.Text("analytics", "country"), Icon = "icon-globe", Route = string.Format("{0}/view/{1}", mainRoute, "country") });
-                treeNodes.Add(new SectionTreeNode() { Id = "ecommerce", Title = ui.Text("analytics", "ecommerce"), Icon = "icon-shopping-basket", Route = string.Format("{0}/view/{1}", mainRoute, "ecommerce") });
-                treeNodes.Add(new SectionTreeNode() { Id = "settings", Title = ui.Text("analytics", "settings"), Icon = "icon-settings", Route = string.Format("{0}/edit/{1}", mainRoute, "settings") });
+
+                var treeFromConfig = GetTreeNodesFromConfig();
+                foreach (var node in treeFromConfig)
+                {
+                    treeNodes.Add(new SectionTreeNode() {
+                        Id = node.Id, 
+                        Title = ui.Text("analytics", node.TitleKey), 
+                        Icon = node.Icon, 
+                        Route = string.Format("{0}/{1}", mainRoute, node.ViewRoute) 
+                    });
+         
+                }
+
+                //treeNodes.Add(new SectionTreeNode() { Id = "views", Title = ui.Text("analytics", "views"), Icon = "icon-activity", Route = string.Format("{0}/view/{1}", mainRoute, "views") });
+                //treeNodes.Add(new SectionTreeNode() { Id = "keywords", Title = ui.Text("analytics", "keywords"), Icon = "icon-tags", Route = string.Format("{0}/view/{1}", mainRoute, "keywords") });
+                //treeNodes.Add(new SectionTreeNode() { Id = "social", Title = ui.Text("analytics", "socialNetwork"), Icon = "icon-chat-active", Route = string.Format("{0}/view/{1}", mainRoute, "social") });
+                //treeNodes.Add(new SectionTreeNode() { Id = "os", Title = ui.Text("analytics", "operatingSystem"), Icon = "icon-windows", Route = string.Format("{0}/view/{1}", mainRoute, "os") });
+                //treeNodes.Add(new SectionTreeNode() { Id = "screenres", Title = ui.Text("analytics", "screenResolution"), Icon = "icon-display", Route = string.Format("{0}/view/{1}", mainRoute, "screenres") });
+                //treeNodes.Add(new SectionTreeNode() { Id = "devices", Title = ui.Text("analytics", "devices"), Icon = "icon-iphone", Route = string.Format("{0}/view/{1}", mainRoute, "devices") });
+                //treeNodes.Add(new SectionTreeNode() { Id = "browser", Title = ui.Text("analytics", "browser"), Icon = "icon-browser-window", Route = string.Format("{0}/view/{1}", mainRoute, "browser") });
+                //treeNodes.Add(new SectionTreeNode() { Id = "language", Title = ui.Text("analytics", "language"), Icon = "icon-chat-active", Route = string.Format("{0}/view/{1}", mainRoute, "language") });
+                //treeNodes.Add(new SectionTreeNode() { Id = "country", Title = ui.Text("analytics", "country"), Icon = "icon-globe", Route = string.Format("{0}/view/{1}", mainRoute, "country") });
+                //treeNodes.Add(new SectionTreeNode() { Id = "ecommerce", Title = ui.Text("analytics", "ecommerce"), Icon = "icon-shopping-basket", Route = string.Format("{0}/view/{1}", mainRoute, "ecommerce") });
+                //treeNodes.Add(new SectionTreeNode() { Id = "settings", Title = ui.Text("analytics", "settings"), Icon = "icon-settings", Route = string.Format("{0}/edit/{1}", mainRoute, "settings") });
 
                 foreach (var item in treeNodes)
                 {
@@ -107,5 +125,52 @@ namespace Analytics.Controllers
 
             return menu;
         }
+
+        
+
+        /// <summary>
+        /// Gets Tree Nodes  from the XML treenodes.config
+        /// </summary>
+        /// <returns>Serializes settings from XML file into a nice list of objects</returns>
+        public List<TreeNodesValue> GetTreeNodesFromConfig()
+        {
+            //A list to store our nodes
+            var treeNodes = new List<TreeNodesValue>();
+
+            //Path to the config file
+            var configPath = HostingEnvironment.MapPath(TreeNodesPath);
+
+            //Load config XML file
+            XmlDocument treeXml = new XmlDocument();
+            treeXml.Load(configPath);
+
+            //Get all child nodes of <AnalyticsTree> node
+            XmlNodeList rootNode = treeXml.SelectNodes("//AnalyticsTree");
+
+            //Ensure we found the <AnalyticsTree> node in the config
+            if (rootNode != null)
+            {
+                //Loop over child nodes inside <AnalyticsTree> node
+                foreach (XmlNode configNode in rootNode)
+                {
+                    foreach (XmlNode treeItem in configNode.ChildNodes)
+                    {
+                        //Go & populate our model from each item in the XML file
+                        var treeToAdd = new TreeNodesValue();
+                        treeToAdd.Id = treeItem.Attributes["Id"].Value;
+                        treeToAdd.TitleKey = treeItem.Attributes["TitleKey"].Value;
+                        treeToAdd.Icon = treeItem.Attributes["Icon"].Value;
+                        treeToAdd.ViewRoute = treeItem.Attributes["ViewRoute"].Value;
+
+                        //Add the item to the list
+                        treeNodes.Add(treeToAdd);
+                    }
+                }
+            }
+
+            //Return the list
+            return treeNodes;
+        }
+
     }
 }
---
+++ Analytics/Models/TreeNodesValue.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Analytics.Models
+{
+    //treenodes.config: <TreeNode Id = "views" TitleKey = "views" Icon = "icon-activity" ViewRoute ="/view/views"/>
+    public class TreeNodesValue
+    {
+        public string Id { get; set; }
+
+        public string TitleKey { get; set; }
+
+        public string Icon { get; set; }
+
+        public string ViewRoute { get; set; }
+    }
+}
