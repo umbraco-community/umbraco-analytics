ad0133c39b8c43dd30c8da4b4b9aeba4bda45369
Warren Buckley <warren@umbraco.com>
2019-01-09 14:39:43 +0000

    Uninstall helper updated as dashboard will disapear when package.manifest file is removed/uninstalled from disk & translations etc

--- Analytics/UninstallHelpers.cs
+++ Analytics/UninstallHelpers.cs
@@ -1,6 +1,4 @@
-﻿using System.Web.Hosting;
-using System.Xml;
-using Umbraco.Web;
+﻿using Umbraco.Web.Composing;
 
 namespace Analytics
 {
@@ -12,69 +10,16 @@ namespace Analytics
         public void RemoveSection()
         {
             //Get the Umbraco Service's Api's
-            var services = UmbracoContext.Current.Application.Services;
+            var sections = Current.Services.SectionService;
 
             //Check to see if the section is still here (should be)
-            var analyticsSection = services.SectionService.GetByAlias("analytics");
+            var analyticsSection = sections.GetByAlias("analytics");
 
             if (analyticsSection != null)
             {
                 //Delete the section from the application
-                services.SectionService.DeleteSection(analyticsSection);
+                sections.DeleteSection(analyticsSection);
             }
         }
-
-        /// <summary>
-        /// Removes our custom translation keys from language files
-        /// </summary>
-        public void RemoveTranslations()
-        {
-            TranslationHelper.RemoveTranslations();
-        }
-
-        /// <summary>
-        /// Removes the XML for the Section Dashboard from the XML file
-        /// </summary>
-        public void RemoveSectionDashboard()
-        {
-            bool saveFile = false;
-
-            //Open up language file
-            //umbraco/config/lang/en.xml
-            var dashboardPath = "~/config/dashboard.config";
-
-            //Path to the file resolved
-            var dashboardFilePath = HostingEnvironment.MapPath(dashboardPath);
-
-            //Load settings.config XML file
-            XmlDocument dashboardXml = new XmlDocument();
-            dashboardXml.Load(dashboardFilePath);
-
-            // Dashboard Root Node
-            // <dashboard>
-            XmlNode dashboardNode = dashboardXml.SelectSingleNode("//dashboard");
-
-            if (dashboardNode != null)
-            {
-                XmlNode findSectionKey = dashboardNode.SelectSingleNode("./section [@alias='AnalyticsDashboardSection']");
-
-                if (findSectionKey != null)
-                {
-                    //Let's remove the key from XML...
-                    dashboardNode.RemoveChild(findSectionKey);
-
-                    //Save the file flag to true
-                    saveFile = true;
-                }
-            }
-
-            //If saveFile flag is true then save the file
-            if (saveFile)
-            {
-                //Save the XML file
-                dashboardXml.Save(dashboardFilePath);
-            }
-        }
-
     }
 }
