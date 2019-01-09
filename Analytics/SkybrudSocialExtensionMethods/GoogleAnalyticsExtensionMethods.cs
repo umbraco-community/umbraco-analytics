1b5c90dd9053b09ace4e2e353b374b49a5218b37
Anders Bjerner <abjerner@skybrud.dk>
2019-01-09 18:28:01 +0100

    Updated extension methods to use Skybrud.Social.Google instead of the older Skybrud.Social
    I'm not sure we actually need the extension methods anymore, but now we can at least build the project :D

--- Analytics/SkybrudSocialExtensionMethods/GoogleAnalyticsExtensionMethods.cs
+++ Analytics/SkybrudSocialExtensionMethods/GoogleAnalyticsExtensionMethods.cs
@@ -4,20 +4,22 @@ using Skybrud.Social;
 using Skybrud.Social.Google.Analytics.Endpoints;
 using Skybrud.Social.Google.Analytics.Endpoints.Raw;
 using Skybrud.Social.Google.Analytics.Models.Profiles;
+using Skybrud.Social.Google.Analytics.Responses.Data;
+using Skybrud.Social.Http;
 
 namespace Analytics.SkybrudSocialExtensionMethods
 {
 
     public static class GoogleAnalyticsExtensionMethods {
 
-        public static string GetData(this AnalyticsRawEndpoint endpoint, AnalyticsProfile profile, DateTime startDate, DateTime endDate, string[] metrics, string[] dimensions, string[] filters, string[] sort) {
+        public static SocialHttpResponse GetData(this AnalyticsDataRawEndpoint endpoint, AnalyticsProfile profile, DateTime startDate, DateTime endDate, string[] metrics, string[] dimensions, string[] filters, string[] sort) {
             return GetData(endpoint, profile.Id, startDate, endDate, metrics, dimensions, filters, sort);
         }
 
-        public static string GetData(this AnalyticsRawEndpoint endpoint, string profileId, DateTime startDate, DateTime endDate, string[] metrics, string[] dimensions, string[] filters, string[] sort) {
+        public static SocialHttpResponse GetData(this AnalyticsDataRawEndpoint endpoint, string profileId, DateTime startDate, DateTime endDate, string[] metrics, string[] dimensions, string[] filters, string[] sort) {
 
             // Initialize the query string
-            NameValueCollection query = new NameValueCollection();
+            SocialHttpQueryString query = new SocialHttpQueryString();
             query.Add("ids", "ga:" + profileId);
             query.Add("start-date", startDate.ToString("yyyy-MM-dd"));
             query.Add("end-date", endDate.ToString("yyyy-MM-dd"));
@@ -28,16 +30,16 @@ namespace Analytics.SkybrudSocialExtensionMethods
             query.Add("access_token", endpoint.Client.AccessToken);
 
             // Make the call to the API
-            return SocialUtils.DoHttpGetRequestAndGetBodyAsString("https://www.googleapis.com/analytics/v3/data/ga", query);
+            return SocialUtils.Http.DoHttpGetRequest("https://www.googleapis.com/analytics/v3/data/ga", query);
 
         }
 
-        public static AnalyticsDataResponse GetData(this AnalyticsEndpoint endpoint, AnalyticsProfile profile, DateTime startDate, DateTime endDate, string[] metrics, string[] dimensions, string[] filters, string[] sort) {
-            return AnalyticsDataResponse.ParseJson(endpoint.Service.Client.Analytics.GetData(profile, startDate, endDate, metrics, dimensions, filters, sort));
+        public static AnalyticsGetDataResponse GetData(this AnalyticsEndpoint endpoint, AnalyticsProfile profile, DateTime startDate, DateTime endDate, string[] metrics, string[] dimensions, string[] filters, string[] sort) {
+            return AnalyticsGetDataResponse.ParseResponse(endpoint.Service.Client.Analytics.Data.GetData(profile, startDate, endDate, metrics, dimensions, filters, sort));
         }
 
-        public static AnalyticsDataResponse GetData(this AnalyticsEndpoint endpoint, string profileId, DateTime startDate, DateTime endDate, string[] metrics, string[] dimensions, string[] filters, string[] sort) {
-            return AnalyticsDataResponse.ParseJson(endpoint.Service.Client.Analytics.GetData(profileId, startDate, endDate, metrics, dimensions, filters, sort));
+        public static AnalyticsGetDataResponse GetData(this AnalyticsEndpoint endpoint, string profileId, DateTime startDate, DateTime endDate, string[] metrics, string[] dimensions, string[] filters, string[] sort) {
+            return AnalyticsGetDataResponse.ParseResponse(endpoint.Service.Client.Analytics.Data.GetData(profileId, startDate, endDate, metrics, dimensions, filters, sort));
         }
 
     }
