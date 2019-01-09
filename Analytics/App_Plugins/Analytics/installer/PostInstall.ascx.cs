17182cb76bdca532be9c7da145282cab3ab42db9
Warren Buckley <warren@umbraco.com>
2019-01-09 16:25:22 +0000

    Yet another WebForm thing - EWWWW

--- Analytics/App_Plugins/Analytics/installer/PostInstall.ascx.cs
+++ Analytics/App_Plugins/Analytics/installer/PostInstall.ascx.cs
@@ -1,25 +1,25 @@
-﻿using System;
-using System.Linq;
-using Umbraco.Web.UI.Controls;
+﻿//using System;
+//using System.Linq;
+//using Umbraco.Web.UI.Controls;
 
-namespace Analytics.Installer
-{
-    public class PostInstall : UmbracoUserControl
-    {
+//namespace Analytics.Installer
+//{
+//    public class PostInstall : UmbracoUserControl
+//    {
 
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            //Get the current user
-            var currentUser = Security.CurrentUser;
+//        protected void Page_Load(object sender, EventArgs e)
+//        {
+//            //Get the current user
+//            var currentUser = Security.CurrentUser;
 
-            //Check if the user has access to the section
-            bool hasAccess = currentUser.AllowedSections.Contains("analytics");
+//            //Check if the user has access to the section
+//            bool hasAccess = currentUser.AllowedSections.Contains("analytics");
 
-            if (!hasAccess)
-            {
-                //Add section if they do not have access to it already
-                currentUser.AddAllowedSection("analytics");
-            }
-        }
-    }
-}
+//            if (!hasAccess)
+//            {
+//                //Add section if they do not have access to it already
+//                currentUser.AddAllowedSection("analytics");
+//            }
+//        }
+//    }
+//}
