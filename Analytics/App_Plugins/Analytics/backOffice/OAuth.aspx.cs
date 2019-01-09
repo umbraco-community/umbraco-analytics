b788f820786f86ccfdac3d40a2ca705c2766ab2c
Warren Buckley <warren@umbraco.com>
2019-01-09 16:24:32 +0000

    Comment out the .ASPX codebehind for now - I FEEL DIRTY

--- Analytics/App_Plugins/Analytics/backOffice/OAuth.aspx.cs
+++ Analytics/App_Plugins/Analytics/backOffice/OAuth.aspx.cs
@@ -1,77 +1,77 @@
-﻿using System;
-using System.Collections.Specialized;
-using System.Linq;
-using System.Web;
-using System.Web.Security;
-using Skybrud.Social;
-using Umbraco.Web.UI.Pages;
-using umbraco;
-using Umbraco.Core.Security;
-using Umbraco.Web;
+﻿//using System;
+//using System.Collections.Specialized;
+//using System.Linq;
+//using System.Web;
+//using System.Web.Security;
+//using Skybrud.Social;
+//using Umbraco.Web.UI.Pages;
+//using umbraco;
+//using Umbraco.Core.Security;
+//using Umbraco.Web;
 
-namespace Analytics.App_Plugins.Analytics.BackOffice {
+//namespace Analytics.App_Plugins.Analytics.BackOffice {
 
-    public partial class OAuth : UmbracoEnsuredPage {
+//    public partial class OAuth : UmbracoEnsuredPage {
 
-        protected override void OnPreInit(EventArgs e) {
+//        protected override void OnPreInit(EventArgs e) {
 
-            base.OnPreInit(e);
+//            base.OnPreInit(e);
             
-            if (AnalyticsHelpers.UmbracoVersion != "7.2.2") return;
+//            if (AnalyticsHelpers.UmbracoVersion != "7.2.2") return;
 
-            // Handle authentication stuff to counteract bug in Umbraco 7.2.2 (see U4-6342)
-            HttpContextWrapper http = new HttpContextWrapper(Context);
-            FormsAuthenticationTicket ticket = http.GetUmbracoAuthTicket();
-            http.AuthenticateCurrentRequest(ticket, true);
+//            // Handle authentication stuff to counteract bug in Umbraco 7.2.2 (see U4-6342)
+//            HttpContextWrapper http = new HttpContextWrapper(Context);
+//            FormsAuthenticationTicket ticket = http.GetUmbracoAuthTicket();
+//            http.AuthenticateCurrentRequest(ticket, true);
         
-        }
+//        }
 
-        protected void Page_Load(object sender, EventArgs e) {
+//        protected void Page_Load(object sender, EventArgs e) {
 
-            //Get current user
-            var currentUser = UmbracoContext.Current.Security.CurrentUser;
+//            //Get current user
+//            var currentUser = UmbracoContext.Current.Security.CurrentUser;
 
-            //Check a user is logged into backoffice
-            if (currentUser == null)
-            {
-                //Ouput an error message
-                Content.Text += ui.Text("analytics", "noAccess");
-                return;
-            }
+//            //Check a user is logged into backoffice
+//            if (currentUser == null)
+//            {
+//                //Ouput an error message
+//                Content.Text += ui.Text("analytics", "noAccess");
+//                return;
+//            }
 
-            //Check the user has access to the analytics section
-            //Prevents anyone with this URL to page from just hitting it
-            if (!currentUser.AllowedSections.Contains("analytics"))
-            {
-                //Ouput an error message
-                Content.Text += ui.Text("analytics", "noAccess");
-                return;
-            }
+//            //Check the user has access to the analytics section
+//            //Prevents anyone with this URL to page from just hitting it
+//            if (!currentUser.AllowedSections.Contains("analytics"))
+//            {
+//                //Ouput an error message
+//                Content.Text += ui.Text("analytics", "noAccess");
+//                return;
+//            }
 
-            // The Analytics authentication site should redirect back to this URI
-            string callback = Request.Url.AbsoluteUri.Replace("/OAuth.aspx", "/OAuthCallback.aspx");
+//            // The Analytics authentication site should redirect back to this URI
+//            string callback = Request.Url.AbsoluteUri.Replace("/OAuth.aspx", "/OAuthCallback.aspx");
 
-            // Generate a random state
-            string state = Guid.NewGuid().ToString();
+//            // Generate a random state
+//            string state = Guid.NewGuid().ToString();
 
-            // Add the state to the session
-            Session.Add("Analytics_" + state, "#h5yr");
+//            // Add the state to the session
+//            Session.Add("Analytics_" + state, "#h5yr");
 
-            // The query string to send to the authentication site
-            NameValueCollection nvc = new NameValueCollection {
-                { "clientcallback", callback },
-                { "clientstate", state },
-                { "lang", UmbracoContext.Current.Security.CurrentUser.Language }
-            };
+//            // The query string to send to the authentication site
+//            NameValueCollection nvc = new NameValueCollection {
+//                { "clientcallback", callback },
+//                { "clientstate", state },
+//                { "lang", UmbracoContext.Current.Security.CurrentUser.Language }
+//            };
 
-            // Generate the URL for the authentication page
-            string oAuthUrl = "http://analytics-oauth.azurewebsites.net/callback/OAuth.aspx?" + SocialUtils.NameValueCollectionToQueryString(nvc);
+//            // Generate the URL for the authentication page
+//            string oAuthUrl = "http://analytics-oauth.azurewebsites.net/callback/OAuth.aspx?" + SocialUtils.NameValueCollectionToQueryString(nvc);
 
-            // Now redirect the user
-            Response.Redirect(oAuthUrl);
+//            // Now redirect the user
+//            Response.Redirect(oAuthUrl);
 
-        }
+//        }
 
-    }
+//    }
 
-}
+//}
