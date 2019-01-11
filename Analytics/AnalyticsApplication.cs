6a756c87c34316cfba450d75c4da2e69b141246d
Warren Buckley <warren@umbraco.com>
2019-01-11 10:22:28 +0000

    Make sure Analytics is last after CMS core ones

--- Analytics/AnalyticsApplication.cs
+++ Analytics/AnalyticsApplication.cs
@@ -5,7 +5,7 @@ namespace Analytics
     /// <summary>
     /// This class is picked up by Umbraco TypeLoader & will register the application/section
     /// </summary>
-    [Application("analytics", "Analytics")]
+    [Application("analytics", "Analytics", 20)]
     public class AnalyticsApplication : IApplication { }
 
 }
